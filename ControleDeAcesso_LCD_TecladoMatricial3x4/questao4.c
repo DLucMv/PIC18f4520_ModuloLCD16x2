5133765821c64132384e7b5910995e9c3499bf5f
DLucMv <dlucasmv@gmail.com>
2022-11-02 18:44:34 -0300

    Adidcionado caracteres especiais e mensagens de acesso negado/liberado

--- ControleDeAcesso_LCD_TecladoMatricial3x4/questao4.c
+++ ControleDeAcesso_LCD_TecladoMatricial3x4/questao4.c
@@ -6,15 +6,29 @@
 #pragma config	PBADEN = OFF
 #pragma config	BORV = 3
 #pragma config 	BOREN = OFF
-	
+
+//Rotinas de atraso	
 void delay_ms (int);
 void delay_s (int);
 void delay_us (int);
 
+//
 void envia_comando (char);
 void envia_dado (char);
+
+//Inicia o display no stado padrao/default
 void inicializar(void);
-void mostrar_LCD(void);
+
+//Caracteres especiais
+void sorriso(void);
+void triste(void);
+void cadeado(void);
+void asterisco(void);
+
+//Mostrar mensagens no display
+void mostrar_saudacao(void);
+void mostrar_senha_invalida(void);
+void mostrar_acesso_liberado(void);
 
 #define comando_dado LATCbits.LATC0
 #define pulso LATCbits.LATC1
@@ -40,6 +54,7 @@ void delay_ms(int ms)
 	}
 }
 
+
 void delay_us(int us)	//funciona a partir de 10us
 {
 	unsigned char i;
@@ -49,6 +64,7 @@ void delay_us(int us)	//funciona a partir de 10us
 	i=0;
 }
 
+
 void envia_comando (char comando)
 {
 		comando_dado = 0;
@@ -57,6 +73,8 @@ void envia_comando (char comando)
 		pulso = 0;
 		delay_ms(10);
 }
+
+
 void envia_dado (char dado)
 {
 		comando_dado = 1;
@@ -66,43 +84,217 @@ void envia_dado (char dado)
 		delay_ms(10);
 }
 
+
 void inicializar(){
 		envia_comando(0b00111000);
 		envia_comando(0b00001100);
 		envia_comando(0b00000001);	
 }
 
-void mostrar_LCD(){
 
-	envia_dado('D');
-	envia_dado('A');
-	envia_dado('V');
-	envia_dado('I');
-	envia_dado('S');
+void sorriso(){
+
+	envia_comando(0x40);
+	envia_dado(0b00010);//1
+	envia_comando(0x41);
+	envia_dado(0b00001);//2
+	envia_comando(0x42);
+	envia_dado(0b11001);//3
+	envia_comando(0x43);
+	envia_dado(0b00001);//4
+	envia_comando(0x44);
+	envia_dado(0b00001);//5
+	envia_comando(0x45);
+	envia_dado(0b11001);//6
+	envia_comando(0x46);
+	envia_dado(0b00010);//7
+	envia_comando(0x47);
+	envia_dado(0b00000);//8
+
+}
+
+
+void triste(){
+
+	envia_comando(0x48);
+	envia_dado(0b00000);//1
+	envia_comando(0x49);
+	envia_dado(0b00001);//2
+	envia_comando(0x4A);
+	envia_dado(0b11010);//3
+	envia_comando(0x4B);
+	envia_dado(0b00010);//4
+	envia_comando(0x4C);
+	envia_dado(0b00010);//5
+	envia_comando(0x4D);
+	envia_dado(0b11010);//6
+	envia_comando(0x4E);
+	envia_dado(0b00001);//7
+	envia_comando(0x4F);
+	envia_dado(0b00000);//8
+
+}
+
+
+void cadeado(){
+
+	envia_comando(0x50);
+	envia_dado(0b01110);//1
+	envia_comando(0x51);
+	envia_dado(0b10001);//2
+	envia_comando(0x52);
+	envia_dado(0b10001);//3
+	envia_comando(0x53);
+	envia_dado(0b11111);//4
+	envia_comando(0x54);
+	envia_dado(0b11011);//5
+	envia_comando(0x55);
+	envia_dado(0b11011);//6
+	envia_comando(0x56);
+	envia_dado(0b11111);//7
+	envia_comando(0x57);
+	envia_dado(0b11111);//8
+
+}
+
+
+void asterisco(){
+
+	envia_comando(0x58);
+	envia_dado(0b00000);//1
+	envia_comando(0x59);
+	envia_dado(0b10101);//2
+	envia_comando(0x5A);
+	envia_dado(0b01110);//3
+	envia_comando(0x5B);
+	envia_dado(0b11111);//4
+	envia_comando(0x5C);
+	envia_dado(0b01110);//5
+	envia_comando(0x5D);
+	envia_dado(0b10101);//6
+	envia_comando(0x5E);
+	envia_dado(0b00000);//7
+	envia_comando(0x5F);
+	envia_dado(0b00000);//8
+
+}
+
+
+void mostrar_saudacao(){
+
+	envia_comando(0b00000001);
+	envia_comando(0x80);
 	envia_dado('O');
-	envia_dado('N');
+	envia_dado('l');
+	envia_dado('a');
+	envia_dado(',');
 	envia_dado(' ');
-	envia_dado('L');
-	envia_dado('U');
-	envia_dado('C');
-	envia_dado('A');
-	envia_dado('S');
+	envia_dado('P');
+	envia_dado('o');
+	envia_dado('r');
+	envia_dado(' ');
+	envia_dado('f');
+	envia_dado('a');
+	envia_dado('v');
+	envia_dado('o');
+	envia_dado('r');
 	envia_comando(0xC0);
 	envia_dado('I');
-	envia_dado('F');
-	envia_dado('C');
-	envia_dado('E');
-	envia_dado('-');
-	envia_dado('F');
-	envia_dado('O');
-	envia_dado('R');
-	envia_dado('T');
-	envia_dado('A');
-	envia_dado('L');
-	envia_dado('E');
-	envia_dado('Z');
-	envia_dado('A');
+	envia_dado('n');
+	envia_dado('s');
+	envia_dado('i');
+	envia_dado('r');
+	envia_dado('a');
+	envia_dado(' ');
+	envia_dado('s');
+	envia_dado('u');
+	envia_dado('a');
+	envia_dado(' ');
+	envia_dado('s');
+	envia_dado('e');
+	envia_dado('n');
+	envia_dado('h');
+	envia_dado('a');
 }
+
+
+void mostrar_senha_invalida(){
+
+	envia_comando(0b00000001);
+	envia_comando(0x80);
+	envia_dado('S');
+	envia_dado('e');
+	envia_dado('n');
+	envia_dado('h');
+	envia_dado('a');
+	envia_dado(' ');
+	envia_dado('i');
+	envia_dado('n');
+	envia_dado('v');
+	envia_dado('a');
+	envia_dado('l');
+	envia_dado('i');
+	envia_dado('d');
+	envia_dado('a');
+	envia_dado('!');
+	envia_comando(0xC0);
+	envia_dado('A');
+	envia_dado('c');
+	envia_dado('e');
+	envia_dado('s');
+	envia_dado('s');
+	envia_dado('o');
+	envia_dado(' ');
+	envia_dado('n');
+	envia_dado('e');
+	envia_dado('g');
+	envia_dado('a');
+	envia_dado('d');
+	envia_dado('o');
+	envia_dado(' ');
+	envia_dado(1);
+}
+
+
+void mostrar_acesso_liberado(){
+
+	envia_comando(0b00000001);
+	envia_comando(0x80);
+	envia_dado('A');
+	envia_dado('c');
+	envia_dado('e');
+	envia_dado('s');
+	envia_dado('s');
+	envia_dado('o');
+	envia_dado(' ');
+	envia_dado('L');
+	envia_dado('i');
+	envia_dado('b');
+	envia_dado('e');
+	envia_dado('r');
+	envia_dado('a');
+	envia_dado('d');
+	envia_dado('o');
+	envia_dado('!');
+	envia_comando(0xC0);
+	envia_dado('S');
+	envia_dado('e');
+	envia_dado('j');
+	envia_dado('a');
+	envia_dado(' ');
+	envia_dado('b');
+	envia_dado('e');
+	envia_dado('m');
+	envia_dado(' ');
+	envia_dado('v');
+	envia_dado('i');
+	envia_dado('n');
+	envia_dado('d');
+	envia_dado('o');
+	envia_dado(' ');
+	envia_dado(0);
+}
+
 
 main ()
 
@@ -121,8 +313,22 @@ main ()
 	
 
 	inicializar();
+	LATBbits.LATB7 = 0;
+	LATCbits.LATC2 = 0;
+	sorriso();
+	triste();
+	cadeado();
+	asterisco();
+	mostrar_saudacao();
+	delay_s(3);
+	mostrar_senha_invalida();
+	LATCbits.LATC2 = 0;
 	LATBbits.LATB7 = 1;
-	mostrar_LCD();
+	delay_s(3);
+	mostrar_acesso_liberado();
+	LATBbits.LATB7 = 0;
+	LATCbits.LATC2 = 1;
+
 	while(1){
 
 	}
